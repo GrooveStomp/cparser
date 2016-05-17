5c1b7df1c55824cb8d83c89e0e04e237050bb0af
GrooveStomp <aaron.oman@gmail.com>
2016-05-17 15:17:48 -0700

    Change default tab spacing to 8 chars

--- file_buffer.c
+++ file_buffer.c
@@ -4,48 +4,50 @@
 #include <stdio.h>
 
 struct buffer {
-  char *Data;
-  size_t Capacity;
-  size_t Length;
+        char *Data;
+        size_t Capacity;
+        size_t Length;
 };
 
 struct buffer *
 BufferSet(struct buffer *Buffer, char *Data, size_t Length, size_t Capacity)
 {
-  Buffer->Data = Data;
-  Buffer->Capacity = Capacity;
-  Buffer->Length = Length;
-  return Buffer;
+        Buffer->Data = Data;
+        Buffer->Capacity = Capacity;
+        Buffer->Length = Length;
+        return(Buffer);
 }
 
 enum copy_file_result
 {
-  COPY_FILE_OK,
-  COPY_FILE_DATA_REMAINING,
-  COPY_FILE_INSUFFICIENT_SPACE,
-  COPY_FILE_ERROR
+        COPY_FILE_OK,
+        COPY_FILE_DATA_REMAINING,
+        COPY_FILE_INSUFFICIENT_SPACE,
+        COPY_FILE_ERROR
 };
 
 enum copy_file_result
 CopyFileIntoBuffer(char *FileName, struct buffer *Mem)
 {
-  FILE *File = fopen(FileName, "r");
-  if(File) {
-    fseek(File, 0, SEEK_END);
-    size_t FileSize = ftell(File);
-    if(FileSize + 1 > Mem->Capacity) {
-      return COPY_FILE_INSUFFICIENT_SPACE;
-    }
+        FILE *File = fopen(FileName, "r");
+        if(File)
+        {
+                fseek(File, 0, SEEK_END);
+                size_t FileSize = ftell(File);
+                if(FileSize + 1 > Mem->Capacity)
+                {
+                        return COPY_FILE_INSUFFICIENT_SPACE;
+                }
 
-    fseek(File, 0, SEEK_SET);
-    fread(Mem->Data, 1, FileSize, File);
-    Mem->Data[FileSize] = 0;
-    Mem->Length = FileSize;
+                fseek(File, 0, SEEK_SET);
+                fread(Mem->Data, 1, FileSize, File);
+                Mem->Data[FileSize] = 0;
+                Mem->Length = FileSize;
 
-    fclose(File);
-  }
+                fclose(File);
+        }
 
-  return(COPY_FILE_OK);
+        return(COPY_FILE_OK);
 }
 
 /*
@@ -55,15 +57,16 @@ CopyFileIntoBuffer(char *FileName, struct buffer *Mem)
 size_t
 FileSize(char *FileName)
 {
-  FILE *File = fopen(FileName, "r");
-  if(File) {
-    fseek(File, 0, SEEK_END);
-    size_t FileSize = ftell(File);
-    fclose(File);
-    return(FileSize + 1); /* +1 for trailing null byte. */
-  }
+        FILE *File = fopen(FileName, "r");
+        if(File)
+        {
+                fseek(File, 0, SEEK_END);
+                size_t FileSize = ftell(File);
+                fclose(File);
+                return(FileSize + 1); /* +1 for trailing null byte. */
+        }
 
-  return(0);
+        return(0);
 }
 
 #endif /* _FILE_BUFFER_C */
--- main.c
+++ main.c
@@ -21,308 +21,308 @@ typedef int bool;
 #define kilobytes(n) (bytes(n) * 1000)
 
 enum token_type {
-  Token_Unknown,
+        Token_Unknown,
 
-  Token_Asterisk,
-  Token_Ampersand,
-  Token_LeftParen,
-  Token_RightParen,
-  Token_LeftBracket,
-  Token_RightBracket,
-  Token_LeftBrace,
-  Token_RightBrace,
-  Token_Colon,
-  Token_SemiColon,
-  Token_Comma,            /*  , */
-  Token_Cross,            /*  + */
-  Token_Dash,             /*  - */
-  Token_Equal,            /*  = */
-  Token_Slash,            /*  / */
-  Token_Dot,              /*  . */
-  Token_Arrow,            /* -> */
+        Token_Asterisk,
+        Token_Ampersand,
+        Token_LeftParen,
+        Token_RightParen,
+        Token_LeftBracket,
+        Token_RightBracket,
+        Token_LeftBrace,
+        Token_RightBrace,
+        Token_Colon,
+        Token_SemiColon,
+        Token_Comma,            /*  , */
+        Token_Cross,            /*  + */
+        Token_Dash,             /*  - */
+        Token_Equal,            /*  = */
+        Token_Slash,            /*  / */
+        Token_Dot,              /*  . */
+        Token_Arrow,            /* -> */
 
-  Token_Character,
-  Token_String,
-  Token_Identifier,
-  Token_Keyword,
+        Token_Character,
+        Token_String,
+        Token_Identifier,
+        Token_Keyword,
 
-  Token_EndOfStream,
+        Token_EndOfStream,
 };
 
 struct token {
-  char *Text;
-  size_t TextLength;
-  enum token_type TokenType;
+        char *Text;
+        size_t TextLength;
+        enum token_type TokenType;
 };
 
 void
 AbortWithMessage(const char *msg)
 {
-  fprintf(stderr, "%s\n", msg);
-  exit(EXIT_FAILURE);
+        fprintf(stderr, "%s\n", msg);
+        exit(EXIT_FAILURE);
 }
 
 void
 Usage()
 {
-  printf("Usage: program file\n");
-  printf("  file: must be a file in this directory\n");
-  printf("  Specify '-h' or '--help' for this help text\n");
-  exit(EXIT_SUCCESS);
+        printf("Usage: program file\n");
+        printf("  file: must be a file in this directory\n");
+        printf("  Specify '-h' or '--help' for this help text\n");
+        exit(EXIT_SUCCESS);
 }
 
 bool
 IsWhitespace(const char character)
 {
-  return(character == ' ' || character == '\r' || character == '\n' || character == '\t');
+        return(character == ' ' || character == '\r' || character == '\n' || character == '\t');
 }
 
 bool
 IsSymbol(const char character)
 {
-  static char symbols[] = {
-    '#', '{', '}', '[', ']', ',', ';', '-', '+', '=', '*', '^', '&', '%', '$', '?', '<', '>', '(', ')', '!',
-    '/', '|', '~', '.', '\'', '"'
-  };
+        static char symbols[] = {
+                '#', '{', '}', '[', ']', ',', ';', '-', '+', '=', '*', '^', '&', '%', '$', '?', '<', '>', '(', ')', '!',
+                '/', '|', '~', '.', '\'', '"'
+        };
 
-  for(int i=0; i < ARRAY_SIZE(symbols); ++i)
-  {
-    if(character == symbols[i]) return(true);
-  }
-  return(false);
+        for(int i=0; i < ARRAY_SIZE(symbols); ++i)
+        {
+                if(character == symbols[i]) return(true);
+        }
+        return(false);
 }
 
 bool
 GetFirstCharacterLiteral(const struct buffer *buff, struct buffer *character)
 {
-  char *string_open_match;
-  char *string_close_match;
+        char *string_open_match;
+        char *string_close_match;
 
-  /* TODO(AARON): Assuming entirety of string fits within both buffer and tmp_buffer. */
-  char tmp_buffer[5] = {0};
-  strncpy(tmp_buffer, buff->Data, min(buff->Length, 4));
+        /* TODO(AARON): Assuming entirety of string fits within both buffer and tmp_buffer. */
+        char tmp_buffer[5] = {0};
+        strncpy(tmp_buffer, buff->Data, min(buff->Length, 4));
 
-  char *match;
-  if((match = strstr(tmp_buffer, "'")) != NULL)
-  {
-    char *close = match + 2;
-    if (*close != '\'') close += 1;
-    if(*close != '\'') return(false);
-    if(*close == '\'' && *(close-1) == '\\') close += 1;
-    if(*close != '\'') return(false);
+        char *match;
+        if((match = strstr(tmp_buffer, "'")) != NULL)
+        {
+                char *close = match + 2;
+                if (*close != '\'') close += 1;
+                if(*close != '\'') return(false);
+                if(*close == '\'' && *(close-1) == '\\') close += 1;
+                if(*close != '\'') return(false);
 
-    character->Data = buff->Data + (match - tmp_buffer);
-    character->Length = close - match + 1;
-    character->Capacity = character->Length;
-    return(true);
-  }
+                character->Data = buff->Data + (match - tmp_buffer);
+                character->Length = close - match + 1;
+                character->Capacity = character->Length;
+                return(true);
+        }
 
-  return(false);
+        return(false);
 }
 
 bool
 GetFirstStringLiteral(const struct buffer *buff, struct buffer *string)
 {
-  char *string_open_match;
-  char *string_close_match;
+        char *string_open_match;
+        char *string_close_match;
 
-  /* TODO(AARON): Assuming entirety of string fits within both buffer and tmp_buffer. */
-  char tmp_buffer[kilobytes(1)] = {0};
-  strncpy(tmp_buffer, buff->Data, min(buff->Length, kilobytes(1)-1));
+        /* TODO(AARON): Assuming entirety of string fits within both buffer and tmp_buffer. */
+        char tmp_buffer[kilobytes(1)] = {0};
+        strncpy(tmp_buffer, buff->Data, min(buff->Length, kilobytes(1)-1));
 
-  if((string_open_match = strstr(tmp_buffer, "\"")) != NULL)
-  {
-    /* TODO(AARON): Handle nested strings. */
-    string_close_match = strstr(tmp_buffer + 1, "\"");
-    char *close_tmp_buffer;
-    while(*(string_close_match - 1) == '\\' || strncmp(string_close_match - 1, "'\"'", 4) == 0)
-    {
-      close_tmp_buffer = string_close_match + 1;
+        if((string_open_match = strstr(tmp_buffer, "\"")) != NULL)
+        {
+                /* TODO(AARON): Handle nested strings. */
+                string_close_match = strstr(tmp_buffer + 1, "\"");
+                char *close_tmp_buffer;
+                while(*(string_close_match - 1) == '\\' || strncmp(string_close_match - 1, "'\"'", 4) == 0)
+                {
+                        close_tmp_buffer = string_close_match + 1;
 
-      if(close_tmp_buffer >= tmp_buffer + min(buff->Length, kilobytes(1)-1))
-      {
-        AbortWithMessage("Couldn't terminate string properly");
-      }
+                        if(close_tmp_buffer >= tmp_buffer + min(buff->Length, kilobytes(1)-1))
+                        {
+                                AbortWithMessage("Couldn't terminate string properly");
+                        }
 
-      string_close_match = strstr(close_tmp_buffer, "\"");
-    }
-    /* TODO(AARON): Raise a big fuss if string_close_match is NULL. */
+                        string_close_match = strstr(close_tmp_buffer, "\"");
+                }
+                /* TODO(AARON): Raise a big fuss if string_close_match is NULL. */
 
-    string->Data = buff->Data + (string_open_match - tmp_buffer);
-    string->Length = string_close_match - string_open_match + 1;
-    string->Capacity = string->Length;
+                string->Data = buff->Data + (string_open_match - tmp_buffer);
+                string->Length = string_close_match - string_open_match + 1;
+                string->Capacity = string->Length;
 
-    return(true);
-  }
+                return(true);
+        }
 
-  return(false);
+        return(false);
 }
 
 bool
 GetFirstComment(const struct buffer *buff, struct buffer *comment)
 {
-  char *comment_open_match;
-  char *comment_close_match;
+        char *comment_open_match;
+        char *comment_close_match;
 
-  /* TODO(AARON): Assuming entirety of comment fits within both buffer and tmp_buffer. */
-  char tmp_buffer[kilobytes(1)] = {0};
-  strncpy(tmp_buffer, buff->Data, min(buff->Length, kilobytes(1)-1));
+        /* TODO(AARON): Assuming entirety of comment fits within both buffer and tmp_buffer. */
+        char tmp_buffer[kilobytes(1)] = {0};
+        strncpy(tmp_buffer, buff->Data, min(buff->Length, kilobytes(1)-1));
 
-  if((comment_open_match = strstr(tmp_buffer, "/*")) != NULL)
-  {
-    /* TODO(AARON): Handle nested comments. */
-    comment_close_match = strstr(tmp_buffer, "*/");
-    /* TODO(AARON): Raise a big fuss if comment_close_match is NULL. */
+        if((comment_open_match = strstr(tmp_buffer, "/*")) != NULL)
+        {
+                /* TODO(AARON): Handle nested comments. */
+                comment_close_match = strstr(tmp_buffer, "*/");
+                /* TODO(AARON): Raise a big fuss if comment_close_match is NULL. */
 
-    comment->Data = buff->Data + (comment_open_match - tmp_buffer);
-    comment->Length = comment_close_match - comment_open_match + 2;
-    comment->Capacity = comment->Length;
+                comment->Data = buff->Data + (comment_open_match - tmp_buffer);
+                comment->Length = comment_close_match - comment_open_match + 2;
+                comment->Capacity = comment->Length;
 
-    return(true);
-  }
+                return(true);
+        }
 
-  return(false);
+        return(false);
 }
 
 bool
 GetFirstKeyword(const struct buffer *buff, struct buffer *keyword)
 {
-  static char *keywords[] = {
-    "auto", "break", "case", "char", "const", "continue", "default", "do", "double", "else", "enum", "extern",
-    "float", "for", "goto", "if", "int", "long", "register", "return", "short", "signed", "sizeof", "static",
-    "struct", "switch", "typedef", "union", "unsigned", "void", "volatile", "while"
-  };
+        static char *keywords[] = {
+                "auto", "break", "case", "char", "const", "continue", "default", "do", "double", "else", "enum", "extern",
+                "float", "for", "goto", "if", "int", "long", "register", "return", "short", "signed", "sizeof", "static",
+                "struct", "switch", "typedef", "union", "unsigned", "void", "volatile", "while"
+        };
 
-  char tmp_buffer[20] = {0}; /* NOTE(AARON): No keyword is even close to 20 characters long. */
-  strncpy(tmp_buffer, buff->Data, min(buff->Length, 19));
+        char tmp_buffer[20] = {0}; /* NOTE(AARON): No keyword is even close to 20 characters long. */
+        strncpy(tmp_buffer, buff->Data, min(buff->Length, 19));
 
-  char *match;
-  for(int i=0; i < ARRAY_SIZE(keywords); ++i)
-  {
-    if((match = strstr(tmp_buffer, keywords[i])) != NULL)
-    {
-      keyword->Data = buff->Data + (match - tmp_buffer);
-      keyword->Length = strlen(keywords[i]);
-      keyword->Capacity = keyword->Length;
-      return(true);
-    }
-  }
+        char *match;
+        for(int i=0; i < ARRAY_SIZE(keywords); ++i)
+        {
+                if((match = strstr(tmp_buffer, keywords[i])) != NULL)
+                {
+                        keyword->Data = buff->Data + (match - tmp_buffer);
+                        keyword->Length = strlen(keywords[i]);
+                        keyword->Capacity = keyword->Length;
+                        return(true);
+                }
+        }
 
-  return(false);
+        return(false);
 }
 
 bool
 GetFirstPreprocessorCommand(const struct buffer *buff, struct buffer *preprocessor)
 {
-  char *match_start;
-  char *match_end;
+        char *match_start;
+        char *match_end;
 
-  char tmp_buffer[kilobytes(1)] = {0};
-  strncpy(tmp_buffer, buff->Data, min(buff->Length, kilobytes(1)-1));
+        char tmp_buffer[kilobytes(1)] = {0};
+        strncpy(tmp_buffer, buff->Data, min(buff->Length, kilobytes(1)-1));
 
-  if((match_start = strstr(tmp_buffer, "#")) != NULL)
-  {
-    for(--match_start; match_start > tmp_buffer && IsWhitespace(*match_start); --match_start);
-    /* Needs to be a newline followed by any amount of whitespace, then '#'. */
-    if(*match_start != '\n' && *match_start != '\0') return(false);
-    ++match_start;
+        if((match_start = strstr(tmp_buffer, "#")) != NULL)
+        {
+                for(--match_start; match_start > tmp_buffer && IsWhitespace(*match_start); --match_start);
+                /* Needs to be a newline followed by any amount of whitespace, then '#'. */
+                if(*match_start != '\n' && *match_start != '\0') return(false);
+                ++match_start;
 
-    match_end = match_start;
+                match_end = match_start;
 
-    while(1)
-    {
-      if(match_end >= tmp_buffer + strlen(tmp_buffer)) return(false);
+                while(1)
+                {
+                        if(match_end >= tmp_buffer + strlen(tmp_buffer)) return(false);
 
-      if(*match_end == '\n' && *(match_end-1) != '\\') break;
+                        if(*match_end == '\n' && *(match_end-1) != '\\') break;
 
-      ++match_end;
-    }
+                        ++match_end;
+                }
 
-    preprocessor->Data = buff->Data + (match_start - tmp_buffer);
-    preprocessor->Length = match_end - match_start;
-    preprocessor->Capacity = preprocessor->Length;
-    return(true);
-  }
+                preprocessor->Data = buff->Data + (match_start - tmp_buffer);
+                preprocessor->Length = match_end - match_start;
+                preprocessor->Capacity = preprocessor->Length;
+                return(true);
+        }
 }
 
 void
 IncrementBuffer(struct buffer *buff, unsigned int count)
 {
-  buff->Data += count;
-  buff->Length -= count;
-  buff->Capacity -= count;
+        buff->Data += count;
+        buff->Length -= count;
+        buff->Capacity -= count;
 }
 
 /* argv[1] is the input file name. */
 int
 main(int argc, char *argv[])
 {
-  if(argc != 2 || strcmp(argv[1], "-h") == 0 || strcmp(argv[1], "--help") == 0) Usage();
+        if(argc != 2 || strcmp(argv[1], "-h") == 0 || strcmp(argv[1], "--help") == 0) Usage();
 
-  size_t AllocSize = FileSize(argv[1]);
-  struct buffer File;
-  BufferSet(&File, (char *)alloca(AllocSize), 0, AllocSize);
+        size_t AllocSize = FileSize(argv[1]);
+        struct buffer File;
+        BufferSet(&File, (char *)alloca(AllocSize), 0, AllocSize);
 
-  if(COPY_FILE_OK != CopyFileIntoBuffer(argv[1], &File))
-  {
-    AbortWithMessage("Couldn't copy entire file to buffer");
-  }
+        if(COPY_FILE_OK != CopyFileIntoBuffer(argv[1], &File))
+        {
+                AbortWithMessage("Couldn't copy entire file to buffer");
+        }
 
-  char print_buffer[kilobytes(1)] = { 0 };
-  int pos = 0;
+        char print_buffer[kilobytes(1)] = { 0 };
+        int pos = 0;
 
-  while(File.Length > 0)
-  {
-    struct buffer match;
-    if(GetFirstKeyword(&File, &match) && match.Data == File.Data)
-    {
-      snprintf(print_buffer, match.Length + strlen("<keyword>") + 1, "<keyword>%s", match.Data);
-      puts(print_buffer);
-      IncrementBuffer(&File, match.Length);
-      pos += match.Length;
-    }
-    else if(GetFirstCharacterLiteral(&File, &match) && match.Data == File.Data)
-    {
-      snprintf(print_buffer, match.Length + strlen("<character literal>") + 1, "<character literal>%s", match.Data);
-      puts(print_buffer);
-      IncrementBuffer(&File, match.Length);
-      pos += match.Length;
-    }
-    else if(GetFirstComment(&File, &match) && match.Data == File.Data)
-    {
-      snprintf(print_buffer, match.Length + strlen("<comment>") + 1, "<comment>%s", match.Data);
-      puts(print_buffer);
-      IncrementBuffer(&File, match.Length);
-      pos += match.Length;
-    }
-    else if(GetFirstStringLiteral(&File, &match) && match.Data == File.Data)
-    {
-      snprintf(print_buffer, match.Length + strlen("<string literal>") + 1, "<string literal>%s", match.Data);
-      puts(print_buffer);
-      IncrementBuffer(&File, match.Length);
-      pos += match.Length;
-    }
-    else if(GetFirstPreprocessorCommand(&File, &match) && match.Data == File.Data)
-    {
-      snprintf(print_buffer, match.Length + strlen("<preprocessor command>") + 1, "<preprocessor command>%s", match.Data);
-      puts(print_buffer);
-      IncrementBuffer(&File, match.Length);
-      pos += match.Length;
-    }
-    else if(IsSymbol(File.Data[0]))
-    {
-      snprintf(print_buffer, 2 + strlen("<symbol>"), "<symbol>%c", File.Data[0]);
-      puts(print_buffer);
-      IncrementBuffer(&File, 1);
-      pos += 1;
-    }
-    else
-    {
-      /* printf("No token found at character [%u]\n", pos); */
-      IncrementBuffer(&File, 1);
-      pos += 1;
-    }
-  }
+        while(File.Length > 0)
+        {
+                struct buffer match;
+                if(GetFirstKeyword(&File, &match) && match.Data == File.Data)
+                {
+                        snprintf(print_buffer, match.Length + strlen("<keyword>") + 1, "<keyword>%s", match.Data);
+                        puts(print_buffer);
+                        IncrementBuffer(&File, match.Length);
+                        pos += match.Length;
+                }
+                else if(GetFirstCharacterLiteral(&File, &match) && match.Data == File.Data)
+                {
+                        snprintf(print_buffer, match.Length + strlen("<character literal>") + 1, "<character literal>%s", match.Data);
+                        puts(print_buffer);
+                        IncrementBuffer(&File, match.Length);
+                        pos += match.Length;
+                }
+                else if(GetFirstComment(&File, &match) && match.Data == File.Data)
+                {
+                        snprintf(print_buffer, match.Length + strlen("<comment>") + 1, "<comment>%s", match.Data);
+                        puts(print_buffer);
+                        IncrementBuffer(&File, match.Length);
+                        pos += match.Length;
+                }
+                else if(GetFirstStringLiteral(&File, &match) && match.Data == File.Data)
+                {
+                        snprintf(print_buffer, match.Length + strlen("<string literal>") + 1, "<string literal>%s", match.Data);
+                        puts(print_buffer);
+                        IncrementBuffer(&File, match.Length);
+                        pos += match.Length;
+                }
+                else if(GetFirstPreprocessorCommand(&File, &match) && match.Data == File.Data)
+                {
+                        snprintf(print_buffer, match.Length + strlen("<preprocessor command>") + 1, "<preprocessor command>%s", match.Data);
+                        puts(print_buffer);
+                        IncrementBuffer(&File, match.Length);
+                        pos += match.Length;
+                }
+                else if(IsSymbol(File.Data[0]))
+                {
+                        snprintf(print_buffer, 2 + strlen("<symbol>"), "<symbol>%c", File.Data[0]);
+                        puts(print_buffer);
+                        IncrementBuffer(&File, 1);
+                        pos += 1;
+                }
+                else
+                {
+                        /* printf("No token found at character [%u]\n", pos); */
+                        IncrementBuffer(&File, 1);
+                        pos += 1;
+                }
+        }
 
-  return(0);
+        return(0);
 }
