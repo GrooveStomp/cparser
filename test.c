766058c6d2ffa11fbced66e2c2070c29d619a7f8
GrooveStomp <aaron.oman@gmail.com>
2016-07-06 14:20:17 -0700

    More tests

--- test.c
+++ test.c
@@ -1,33 +1,38 @@
 #include <stdlib.h> /* EXIT_SUCCESS, EXIT_FAILURE */
 #include <stdio.h>
+#include <alloca.h>
 
 #include "string.c"
 #include "lexer.c"
 #include "parser.c"
 
-#define Assert(Description, Expression) AssertFn((Description), (Expression), __LINE__, __func__)
+typedef bool (*parser_function)(struct tokenizer *);
 
-void AssertFn(const char *Description, int Expression, int LineNumber, const char *FuncName)
+#define Assert(Description, Expression) AssertFn((Description), (Expression), __LINE__, __func__)
+#define Test(Function, String) \
+        { \
+                struct tokenizer Tokenizer = InitTokenizer((String)); \
+                bool Result = Function(&Tokenizer); \
+                AssertFn("Result should be true", Result == true, __LINE__, __func__); \
+                AssertFn("Tokenizer advances to end of string", Tokenizer.At == (String) + StringLength((String)), __LINE__, __func__); \
+        }
+
+void
+AssertFn(const char *Description, int Expression, int LineNumber, const char *FuncName)
 {
         if(!Expression)
         {
+                char *Suffix = "";
                 if(Description != NULL)
                 {
-                        fprintf(stderr, "Assertion failed in %s at line #%d: %s\n", FuncName, LineNumber, Description);
+                        size_t AllocSize = StringLength((char *)Description) + StringLength(": ");
+                        Suffix = (char *)alloca(AllocSize);
+                        snprintf(Suffix, AllocSize + 1, ": %s", Description);
                 }
-                else
-                {
-                        fprintf(stderr, "Assertion failed in %s at line #%d\n", FuncName, LineNumber);
-                }
+                fprintf(stderr, "Assertion failed in %s() at line #%d%s\n", FuncName, LineNumber, Suffix);
+
                 exit(EXIT_FAILURE);
         }
-}
-
-void
-AbortWithMessage(const char *msg)
-{
-        fprintf(stderr, "%s\n", msg);
-        exit(EXIT_FAILURE);
 }
 
 struct tokenizer
@@ -39,56 +44,71 @@ InitTokenizer(char *String)
         return(Tokenizer);
 }
 
+/*----------------------------------------------------------------------------
+  Tests
+  ----------------------------------------------------------------------------*/
+
+void
+TestConstant()
+{
+        parser_function Fn = ParseConstant;
+        Test(Fn, "1");   /* integer-constant */
+        Test(Fn, "'c'"); /* character-constant */
+        Test(Fn, "2.0"); /* floating-constant */
+        /* TODO(AARON): enumeration-constant */
+}
+
+void
+TestArgumentExpressionList()
+{
+        parser_function Fn = ParseArgumentExpressionList;
+        Test(Fn, "(int)4.0");           /* assignment-expression */
+        Test(Fn, "(int)4.0, (int)4.0"); /* argument-expression-list , assignment-expression */
+}
+
 void
 TestUnaryExpression()
 {
-        struct tokenizer Tokenizer;
-        bool Result;
-        char *String;
+        parser_function Fn = ParseUnaryExpression;
+        Test(Fn, "\"String\"");    /* postfix-expression */
+        Test(Fn, "++Foo");         /* ++ unary-expression */
+        Test(Fn, "--Foo");         /* -- unary-expression */
+        Test(Fn, "&(int)4.0");     /* unary-operator cast-expression */
+        Test(Fn, "sizeof(Foo++)"); /* sizeof unary-expression */
+        Test(Fn, "sizeof(int)");   /* sizeof ( type-name ) */
+}
 
-        String = "sizeof ( int )"; /* identifier | constant | string )"; */
-        Tokenizer = InitTokenizer(String);
-        Result = ParseUnaryExpression(&Tokenizer);
-        Assert("Result should be true", Result == true);
-        Assert("Tokenizer advances to end of string", Tokenizer.At == String + StringLength(String));
+void
+TestCastExpression()
+{
+        parser_function Fn = ParseCastExpression;
+        Test(Fn, "sizeof(int)"); /* unary-expression */
+        Test(Fn, "(int)4.0");    /* ( type-name ) cast-expression */
+}
+
+void
+TestMultiplicativeExpression()
+{
+        parser_function Fn = ParseMultiplicativeExpression;
+        Test(Fn, "(int)4.0");            /* cast-expression */
+        Test(Fn, "(int)4.0 * (int)4.0"); /* multiplicative-expression * cast-expression */
+        Test(Fn, "(int)4.0 / (int)4.0"); /* multiplicative-expression / cast-expression */
+        Test(Fn, "(int)4.0 % (int)4.0"); /* multiplicative-expression % cast-expression */
 }
 
 void
 TestPrimaryExpression()
 {
-        struct tokenizer Tokenizer;
-        bool Result;
-        char *String;
-
-        String = "my_var";
-        Tokenizer = InitTokenizer(String);
-        Result = ParsePrimaryExpression(&Tokenizer);
-        Assert("Result should be true", Result == true);
-        Assert("Tokenizer advances to end of string", Tokenizer.At == String + StringLength(String));
-
-        String = "4.0";
-        Tokenizer = InitTokenizer(String);
-        Result = ParsePrimaryExpression(&Tokenizer);
-        Assert("Result should be true", Result == true);
-        Assert("Tokenizer advances to end of string", Tokenizer.At == String + StringLength(String));
-
-        String = "\"This is a string\"";
-        Tokenizer = InitTokenizer(String);
-        Result = ParsePrimaryExpression(&Tokenizer);
-        Assert("Result should be true", Result == true);
-        Assert("Tokenizer advances to end of string", Tokenizer.At == String + StringLength(String));
-
-        String = "( sizeof(int) )";
-        Tokenizer = InitTokenizer(String);
-        Result = ParsePrimaryExpression(&Tokenizer);
-        Assert("Result should be true", Result == true);
-        Assert("Tokenizer advances to end of string", Tokenizer.At == String + StringLength(String));
+        parser_function Fn = ParsePrimaryExpression;
+        Test(Fn, "my_var");        /* identifier */
+        Test(Fn, "4.0");           /* constant */
+        Test(Fn, "\"String\"");    /* string */
+        Test(Fn, "(sizeof(int))"); /* ( expression ) */
 }
 
 /*----------------------------------------------------------------------------
   Main Entrypoint
   ----------------------------------------------------------------------------*/
-
 void
 Usage()
 {
@@ -110,7 +130,11 @@ main(int ArgCount, char **Args)
                 }
         }
 
+        TestConstant();
+        TestArgumentExpressionList();
         TestUnaryExpression();
+        TestCastExpression();
+        TestMultiplicativeExpression();
         TestPrimaryExpression();
 
         printf("All tests passed successfully\n");
